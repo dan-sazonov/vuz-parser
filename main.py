0d9596c7f2de8a2f7da8da1ebd694228ca47a922
Dan Sazonov <p.294803@gmail.com>
2022-07-10 21:04:56 +0300

    Тестовый парсер ГУАП

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import config
+import requests
+from bs4 import BeautifulSoup
 
 c = config.ColorMethods()
 
 
+def scrap_guap():
+    url = 'https://priem.guap.ru/_lists/List_1693_14'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    data_raw = str(soup.select('#tablestat tbody')[0])
+    data_arr = []
+
+    for abt in data_raw.split('</tr><tr class="warning">'):
+        data_arr.append(int(abt.split('</td><td>')[2]))
+
+    return sorted(data_arr, reverse=True)
+
+
 def main():
-    pass
+    print(scrap_guap())
 
 
 if __name__ == '__main__':
