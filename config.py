1eae828793350b06e64d12d3ddea1056e7ad56be
Dan Sazonov <p.294803@gmail.com>
2022-07-10 20:46:51 +0300

    Основная структура

---
+++ config.py
@@ -0,0 +1,11 @@
+import colorama
+
+
+class ColorMethods:
+    def __init__(self):
+        colorama.init()
+        self.reset = colorama.Style.RESET_ALL
+        self.red = colorama.Fore.RED
+        self.green = colorama.Fore.GREEN
+        self.bright = colorama.Style.BRIGHT
+        self.dim = colorama.Style.DIM
---
+++ main.py
@@ -0,0 +1,11 @@
+import config
+
+c = config.ColorMethods()
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
