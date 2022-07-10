6a376be79c09eda619ddfafeb08ae43597063e79
Dan Sazonov <p.294803@gmail.com>
2022-07-10 21:13:49 +0300

    Тестовая функция для дерганья парсера

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import config
 import requests
 from bs4 import BeautifulSoup
+
+import config
 
 c = config.ColorMethods()
 
 
-def scrap_guap():
-    url = 'https://priem.guap.ru/_lists/List_1693_14'
+def scrap_guap(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
     data_raw = str(soup.select('#tablestat tbody')[0])
@@ -18,8 +18,19 @@ def scrap_guap():
     return sorted(data_arr, reverse=True)
 
 
+def get_guap():
+    specialties = {
+        'Информационные системы и технологии': 'https://priem.guap.ru/_lists/List_1698_14',
+        'Информатика и вычислительная техника': 'https://priem.guap.ru/_lists/List_1693_14'
+    }
+    for spec in specialties:
+        print(f"{spec}: {scrap_guap(specialties[spec])}")
+
+
 def main():
-    print(scrap_guap())
+    usr_input = int(input('Введите номер вуза: '))
+    if usr_input == 1:
+        get_guap()
 
 
 if __name__ == '__main__':
